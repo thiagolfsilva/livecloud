46045dc971a78cc29cc0f69bf591488c5799558f
Thiago da Silva <thiagosilva@uni.minerva.edu>
2023-04-02 16:31:52 +0800

    Notification debug

--- notifications/main.py
+++ notifications/main.py
@@ -9,12 +9,17 @@ data_url = "https://3qfclxl7ne2ttf4jxzpreebb3i0popbm.lambda-url.eu-west-3.on.aws
 
 db = firestore.Client()
 
-def telegram_notifications(request):
+def telegram_notifications(request, context):
     # Call the web API and get the data
-    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    response = requests.get(data_url)
+    print('response')
+    print(response)
 
     # Parse the response as JSON
     result = json.loads(response.content)
+    print('result')
+    print(result)
+
 
     # Baseline cost of trading fees. An arbitrage is only profitable if the price difference is bigger than this percentage
     threshold = 0.2
@@ -22,7 +27,7 @@ def telegram_notifications(request):
     # Add to the message all coins that are profitable to arbitrage using taker orders
     message = f'Coins with profitable arbitrage opportunities:\n'
     
-    for item in response:
+    for item in result:
         if item['profit']>threshold:
             message+= f"{item['coin']} - Buy on {item['lowest_ask_venue']} for ${item['lowest_ask']:.4f}, Sell on {item['highest_bid_venue']} for ${item['highest_bid']:.4f}, instant {(item['profit']-threshold):.2f}% profit\n"
     message+='Considering 0.1% trading fees on each side'
@@ -40,3 +45,6 @@ def telegram_notifications(request):
 def send_message(chat_id, text):
     data = {'chat_id': chat_id, 'text': text}
     requests.post(f'{api_url}/sendMessage', json=data)
+
+if __name__ == '__main__':
+    telegram_notifications(0,0)
